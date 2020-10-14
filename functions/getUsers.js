3a1450984ca0b9b9cb462a36b44bc20a66ff163b
Valentin Fedyakov <dagot32167@gmail.com>
2020-10-15 00:24:22 +0300

    add mock data generator

--- functions/getUsers.js
+++ functions/getUsers.js
@@ -1,6 +1,37 @@
+const mocker = require('mocker-data-generator').default;
+const uuid = () =>'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,(c,r)=>('x'==с?(r=Math.random()*16|0):(r&0x3|0x8)).toString(16));
+
+const user = {
+    id: {
+        function: function() {
+            return uuid();
+        }
+    },
+    firstName: {
+        faker: 'name.firstName'
+    },
+    lastName: {
+        faker: 'name.lastName'
+    },
+    group: {
+        values: ['Management', 'Accounting', 'Human Resources', 'IT']
+    },
+    experience: {
+        function: function() {
+            return Math.floor(Math.random() * Math.floor(5));
+        }
+    }
+}
+
 exports.handler = async (event, context) => {
     return {
         statusCode: 200,
-        body: "Hello, World"
+        body: JSON.stringify({
+            ...mocker().schema('user', user, 20).buildSync(),
+            sortFields: [
+                'group',
+                'experience'
+            ]
+        })
     };
 };
